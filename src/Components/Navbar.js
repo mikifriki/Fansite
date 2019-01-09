5b731aa47021ac35f849bc1bc790563e7a7ea27f
mikifriki <mihkel.liblikman@gmail.com>
2019-01-09 16:38:11 +0200

    Added title effect

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ class Picturebox extends React.Component {
 	render() {
 		return (
 			<div className="Topnav">
-				<a className="active" href="/Components/index.html">Home</a>
+				<a href="https://mikifriki.github.io/Fansite/">Home</a>
 				<p><a href="https://store.steampowered.com/news/?appids=22340">News</a></p>
 				<p><a href="https://store.steampowered.com/app/22340/Call_of_Cthulhu_Dark_Corners_of_the_Earth/">Buy</a></p>
 				<p><a href="https://en.wikipedia.org/wiki/Call_of_Cthulhu:_Dark_Corners_of_the_Earth">More info</a></p>
