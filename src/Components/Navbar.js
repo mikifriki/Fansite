7967b7390e53ef65ecb55a8673233ab91c7a1d3d
mikifriki <mihkel.liblikman@gmail.com>
2019-01-09 17:21:34 +0200

    NavBar update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -10,6 +10,7 @@ class Picturebox extends React.Component {
 				<p><a href="https://store.steampowered.com/news/?appids=22340">News</a></p>
 				<p><a href="https://store.steampowered.com/app/22340/Call_of_Cthulhu_Dark_Corners_of_the_Earth/">Buy</a></p>
 				<p><a href="https://en.wikipedia.org/wiki/Call_of_Cthulhu:_Dark_Corners_of_the_Earth">More info</a></p>
+				<a href="https://steamcommunity.com/app/22340">Forums</a>
 			</div>
 		);
 	}
